981ea5d1a2a4557f90cdf8c826f623a9d4bb0be9
Justin Kay <justinkay92@gmail.com>
2014-04-08 10:40:42 -0700

    removed prints

--- test_forest.py
+++ test_forest.py
@@ -2,22 +2,28 @@ from decision_tree import *
 from forest import *
 from numpy import array
 import scipy.io as sio
+import csv
 
 all_data = sio.loadmat('spam.mat')
 data = all_data['Xtrain'][:,:]
 labels = all_data['ytrain'][:]
 
-f = forest(data[0:3200,:], labels[0:3200], 100)
-score = 0
-for i in range(3200, 3450):
-    score += f.classify(data[i,:]) == labels[i][0]
+#f = forest(data[0:3200,:], labels[0:3200], 100)
+#score = 0
+#for i in range(3200, 3450):
+#    score += f.classify(data[i,:]) == labels[i][0]
 
-print 'score = ' + str(score)
-print 'error = ', str(1 - float(score) / 250)
+#print 'score = ' + str(score)
+#print 'error = ', str(1 - float(score) / 250)
 
+print len(all_data['Xtest'])
 f = forest(data, labels, 100)
 predictions = []
-for i in range(len(data)):
-    predictions.append(f.classify(all_data['Xtest'][i,:]))
+for i in range(len(all_data['Xtest'])):
+    predictions.append([i+1, f.classify(all_data['Xtest'][i,:])])
 
 print predictions
+
+with open('forestpredictions.csv', 'w', newline='') as fp:
+    a = csv.writer(fp, delimiter=',')
+    a.writerows(predictions)
